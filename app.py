1d38300fef25562af5cfb613098488f151f85873
Sriram-bb63 <invadersriram2002@gmail.com>
2022-01-27 15:58:58 +0530

    flask folder structure; connect app with html

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def main():
-    return "hello world"
+    if request.method == "POST":
+        username = request.form.get("username")
+        print(username)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
